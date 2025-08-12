208e19efc6f2e3be4504a81afc09becc598efa3a
Francisco Cejas <francejas98@gmail.com>
2025-08-11 22:28:58 -0300

    g

---
+++ Practica/Guia 0/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numN = 10;
+        double numA = 10.4;
+        char numC = 'c';
+
+        System.out.println("Valor de numN: " + numN);
+        System.out.println("Valor de numA: " + numA);
+        System.out.println("Valor de numC: " + numC);
+    }
+}
