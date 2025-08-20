8403287bcd34f9697f49484740cf001cae4b2efa
Francisco Cejas <138420593+francejas@users.noreply.github.com>
2025-08-20 10:20:38 -0300

    Update Main.java

--- Practica/Guia 3/src/Main.java
+++ Practica/Guia 3/src/Main.java
@@ -1,15 +1,99 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package Empleado;
+
+public class Empleado {
+    public String dni;
+    public String nombre;
+    public String apellido;
+    public Double salario;
+
+
+    public Empleado(String dni, String nombre, String apellido,Double salario){
+        this.dni=dni;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.salario=salario;
+
+    }
+
+    public Empleado(String dni, String nombre, String apellido, int salario) {
+        this.dni=dni;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.salario=(double)salario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Double salario) {
+        this.salario = salario;
+    }
+
+    public double calcularSalarioAnual(){
+        return salario*12;
+    }
+
+    public void aumentarSalario(double porcentaje){
+        salario=salario+(salario*porcentaje/100);
+    }
+
+
+
+
+}
+import Empleado.Empleado;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Empleado empleado1 =new Empleado("23456345","Carlos","Gutiérrez",25000);
+        Empleado empleado2 =new Empleado("34234123","Ana","Sánchez,",27500);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Empleado 1:");
+        System.out.println("Nombre: "+ empleado1.getNombre());
+        System.out.println("Apellido: "+ empleado1.getApellido());
+        System.out.println("DNI: "+ empleado1.getDni());
+        System.out.println("Salario: "+ empleado1.getSalario());
+
+        //System.out.println("Empleado 1: Nombre: "+ empleado1.getNombre() + "Apellido: "+empleado1.getApellido()+);
+        System.out.println("");
+        System.out.println("Empleado 2:");
+        System.out.println("Nombre: "+ empleado2.getNombre());
+        System.out.println("Apellido: "+ empleado2.getApellido());
+        System.out.println("DNI: "+ empleado2.getDni());
+        System.out.println("Salario: "+ empleado2.getSalario());
+
+        System.out.println("Salario inicial de Empleado 1:"+ empleado1.getSalario());
+        System.out.println("Salario anual de Empleado 1:"+ empleado1.calcularSalarioAnual());
+        empleado1.aumentarSalario(15.0);
+        System.out.println("Salario aumentado de Empleado 1:"+ empleado1.getSalario());
+        System.out.println("Salario anual aumentado de Empleado 1:"+ empleado1.calcularSalarioAnual());
+
     }
 }
