bdcf2710af89f08b88adb8d629a45ff627242277
Francisco Cejas <francejas98@gmail.com>
2025-08-31 02:23:07 -0300

    s

--- Practica/Guia 5/3/src/Circulo.java
+++ Practica/Guia 5/3/src/Circulo.java
@@ -1,6 +1,11 @@
+Definir la clase Círculo, que posee como atributo un radio cuyo valor por defecto al
+ser inicializado sin valores es 1.0. Además el tipo Círculo posee un atributo color, por
+        defecto rojo, un método para calcular el área y otro para imprimir sus características.
 public class Circulo {
+    private double radio;
 
-    
+
+
 
 
 
