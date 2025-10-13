cf7017e48bb6062d6b82db672a41be5dee43515f
Francisco Cejas <francejas98@gmail.com>
2025-10-13 17:00:24 -0300

    hj

--- Practica/Guia 9/src/Main.java
+++ Practica/Guia 9/src/Main.java
@@ -1,11 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
        //1
+
+        /*
         Random rand = new Random();
 
         ArrayList<Integer> arrayList =  new ArrayList<>();
@@ -19,9 +18,40 @@ public class Main {
 
         System.out.println("ArrayList original desordenado:");
         System.out.println(arrayList);
-        Collections.sort(arrayList);
-        System.out.println("ArrayList original ordenado:");
 
+        Collections.sort(arrayList);
+
+        System.out.println("\nArrayList original ordenado (con duplicados):");
+        System.out.println(arrayList);
+
+        hashSet.addAll(arrayList);
+        System.out.println("\nHashSet con elementos únicos (sin duplicados):");
+        System.out.println(hashSet);
+
+
+         */
+
+        ArrayList<Integer> arrayListIntRand = new ArrayList<>();
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        int limite = 20;
+
+        Random random = new Random();
+
+
+        for (int i = 0; i < limite ; i++) {
+            arrayListIntRand.add(random.nextInt(11));
+        }
+
+        System.out.println("Lista original generada (con duplicados):");
+        System.out.println(arrayListIntRand);
+
+        treeSet.addAll(arrayListIntRand);
+        System.out.println("\nNúmeros en orden ascendente (usando TreeSet):");
+        System.out.println(treeSet);
+        System.out.println("\nNúmeros en orden descendente (usando descendingSet()):");
+        System.out.println(treeSet.descendingSet());
 
 
 
