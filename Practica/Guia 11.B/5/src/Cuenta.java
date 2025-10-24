fbd1b0ba3364e69b06705806e3c61c7736fb2424
Francisco Cejas <francejas98@gmail.com>
2025-10-24 18:29:41 -0300

    fdg

---
+++ Practica/Guia 11.B/5/src/Cuenta.java
@@ -0,0 +1,12 @@
+public class Cuenta {
+    private int saldo;
+    private int numeroCuenta;
+
+    public Cuenta(int saldo, int numeroCuenta) {
+        this.saldo = saldo;
+        this.numeroCuenta = numeroCuenta;
+    }
+    
+
+
+}
