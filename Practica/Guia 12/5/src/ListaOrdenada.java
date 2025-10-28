0b927146da8646d0c966959794b2f3c0dde304d3
Francisco Cejas <francejas98@gmail.com>
2025-10-28 03:39:10 -0300

    sdf

---
+++ Practica/Guia 12/5/src/ListaOrdenada.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaOrdenada<T extends Comparable<T>>  {
+
+    private List<T> list;
+
+    public ListaOrdenada(List<T> list) {
+        this.list = new ArrayList<>();
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    public void agregar (T elemento){
+        int indiceDeInsercion
+    }
+
+
+
+}
