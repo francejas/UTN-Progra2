2bf3a97e975f8198f97fffc12e12794c24b80703
Francisco Cejas <francejas98@gmail.com>
2025-10-29 21:31:26 -0300

    sdf

--- Practica/Guia 13.2/src/Main.java
+++ Practica/Guia 13.2/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        GestorJSONPersona gestorJSONPersona = new GestorJSONPersona();
+        Persona persona = new Persona("Pepe", 15, "43223223", "masculino");
+        gestorJSONPersona.personaArchivo(persona);
+        // gestorPersona.serializar() convierte el objeto a JSONObject
+        // y el metodo estatico grabar() guarda el JSONObject en un archivo con extension .json
+
+
+
+        /// EJERCICIO 2 - Lee el archivo persona.json que creaste en el ejerc anterior
+        // y convierte el JSON leído de nuevo a un objeto Persona.
+        // Objetivos: Leer datos JSON desde un archivo y Convertir JSON a un objeto Java.
+
+        Persona personaLeida= gestorJSONPersona.archivoPersona();
+        // gestorPersona.deserializar() recibe el JSONTokener generado al leer el archivo JSON,
+        // para poder convertir el JSONObject a un objeto Java
+        System.out.println("Persona leida del JSON: " + personaLeida.toString());
 
 
 
--- Practica/Guia 13.2/src/OperacionLectoEscritura.java
+++ Practica/Guia 13.2/src/OperacionLectoEscritura.java
@@ -1,10 +1,45 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
+import java.io.*;
 import java.time.Period;
 
 public final class OperacionLectoEscritura {
     public OperacionLectoEscritura() {
     }
 
-    public static void grabar (String nombreArchivo, )
+    public static void grabar (String nombreArchivo, JSONObject jsonObject ){
+        try {
+            FileWriter fileWriter = new FileWriter(nombreArchivo);
+            fileWriter.write(jsonObject.toString(4));
+            fileWriter.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void grabar (String nombreArchivo, JSONArray jsonArray){
+        try {
+            FileWriter fileWriter = new FileWriter(nombreArchivo);
+            fileWriter.write(jsonArray.toString(4));
+            fileWriter.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONTokener leer (String nombreArchivo){
+        JSONTokener tokener = null;
+        try {
+            tokener = new JSONTokener(new FileReader(nombreArchivo));
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return tokener;
+    }
+
 }
